395abb67be6acdb95a4c433bd282c28bec09e82a
Russell Cheung <russell@replo.de>
2025-02-28 23:17:58 +0100

    Add editorconfig

--- bin/put-tfstate.sh
+++ bin/put-tfstate.sh
@@ -3,5 +3,5 @@
 set -e
 
 aws s3api put-object --bucket infra --key terraform.tfstate --body terraform.tfstate \
-	--endpoint-url $CLOUDFLARE_R2_ENDPOINT \
-	--checksum-algorithm CRC32
+  --endpoint-url $CLOUDFLARE_R2_ENDPOINT \
+  --checksum-algorithm CRC32
