844a0a38124e1492cb1430106d680b0c03e9311b
Russell Cheung <russell@replo.de>
2025-02-28 22:51:38 +0100

    Add shell script and yaml linting

--- bin/butane.sh
+++ bin/butane.sh
@@ -4,12 +4,12 @@
 set -e
 
 if [ ! -f /usr/local/bin/butane ]; then
-  wget -q -O /usr/local/bin/butane \
-    https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu
-  wget -q https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu.asc
-  curl -s https://fedoraproject.org/fedora.gpg | gpg --import
-  gpg --verify butane-x86_64-unknown-linux-gnu.asc /usr/local/bin/butane
-  chmod +x /usr/local/bin/butane
+	wget -q -O /usr/local/bin/butane \
+		https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu
+	wget -q https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu.asc
+	curl -s https://fedoraproject.org/fedora.gpg | gpg --import
+	gpg --verify butane-x86_64-unknown-linux-gnu.asc /usr/local/bin/butane
+	chmod +x /usr/local/bin/butane
 fi
 
 IGN=$(butane $@)
--- bin/get-tfstate.sh
+++ bin/get-tfstate.sh
@@ -3,5 +3,5 @@
 set -e
 
 aws s3api get-object --bucket infra --key terraform.tfstate \
-  --endpoint-url $CLOUDFLARE_R2_ENDPOINT \
-  terraform.tfstate
+	--endpoint-url $CLOUDFLARE_R2_ENDPOINT \
+	terraform.tfstate
