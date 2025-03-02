6fed0b5d876087811afa3377163da55427e6e10a
Russell Cheung <russell@replo.de>
2025-03-02 23:31:38 +0100

    Refactor butane script

--- bin/butane.sh
+++ bin/butane.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
-# https://search.opentofu.org/provider/opentofu/external/latest/docs/datasources/external#processing-json-in-shell-scripts
 
 set -e
 
-if [ ! -f /usr/local/bin/butane ]; then
-  wget -q -O /usr/local/bin/butane \
+download_butane() {
+  BUTANE="$PWD/build/butane"
+  BUTANE_PATH="$(dirname $BUTANE)"
+
+  if [ -f "$BUTANE" ]; then
+    return 0
+  fi
+
+  if [ ! -d "$BUTANE_PATH" ]; then
+    mkdir -p "$BUTANE_PATH"
+  fi
+
+  wget -q -O $BUTANE \
     https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu
   wget -q https://github.com/coreos/butane/releases/download/v0.23.0/butane-x86_64-unknown-linux-gnu.asc
   curl -s https://fedoraproject.org/fedora.gpg | gpg --import
-  gpg --verify butane-x86_64-unknown-linux-gnu.asc /usr/local/bin/butane
-  chmod +x /usr/local/bin/butane
+  gpg --verify butane-x86_64-unknown-linux-gnu.asc $BUTANE
+  chmod +x $BUTANE
+  rm butane-x86_64-unknown-linux-gnu.asc
+}
+
+wrap_ignition() {
+  # https://search.opentofu.org/provider/opentofu/external/latest/docs/datasources/external#processing-json-in-shell-scripts
+  jq -nc --arg ign "$1" '{"ign":$ign}'
+}
+
+BUTANE=$(which butane)
+
+if [ ! -z "$BUTANE" ]; then
+  wrap_ignition "$($BUTANE $@)"
+  exit 0
 fi
 
-IGN=$(butane $@)
-
-jq -nc --arg ign "$IGN" '{"ign":$ign}'
+download_butane
+wrap_ignition "$($BUTANE $@)"
