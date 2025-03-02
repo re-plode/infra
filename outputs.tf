35eec93f848e31b3f6f2ec061d903502b9392266
Russell Cheung <russell@replo.de>
2025-03-03 00:48:55 +0100

    Revert "Add mailgun domain"
    This reverts commit 260aa808ff1fb8785b10e9376b96fa1fe33108ea.

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,3 @@
 output "hcloud_internal_net_ip_addr" {
   value = hcloud_server.internal_net.ipv4_address
 }
-
-output "mailgun_smtp_login" {
-  value = mailgun_domain.replode.smtp_login
-}
-output "mailgun_smtp_password" {
-  value     = mailgun_domain.replode.smtp_password
-  sensitive = true
-}
-output "mailgun_receiving_records" {
-  value = mailgun_domain.replode.receiving_records_set
-}
-output "mailgun_sending_records" {
-  value = mailgun_domain.replode.sending_records_set
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "mailgun_api_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "mailgun_smtp_password" {
-  type      = string
-  sensitive = true
-}
