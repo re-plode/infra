ffda0604766fce0281b2dc7028d5665312ac44a8
Russell <russell@replo.de>
2026-04-05 13:29:21 +0200

    Set up docker-ce hetzner image

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 5"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3"
+    }
   }
 }
 
@@ -22,6 +26,10 @@ provider "synology" {
   skip_cert_check = true
 }
 
+provider "docker" {
+  host = "ssh://root@${hcloud_server.internal_net.ipv4_address}"
+}
+
 resource "hcloud_ssh_key" "fedora" {
   name       = "russellc@fedora"
   public_key = file("config/ssh/id_ed25519_fedora.pub")
@@ -29,15 +37,6 @@ resource "hcloud_ssh_key" "fedora" {
 resource "hcloud_ssh_key" "ipadpro" {
   name       = "russellc@ipadpro"
   public_key = file("config/ssh/id_ed25519_ipadpro.pub")
-}
-
-data "hcloud_image" "coreos_snapshot" {
-  with_selector = "os_family=fedora,os_flavor=coreos,os_arch=x86_64"
-  most_recent   = true
-}
-
-data "external" "ignition" {
-  program = ["./bin/butane.sh", "--files-dir", ".", "coreos/internal-net.bu"]
 }
 
 resource "hcloud_firewall" "internal_net_firewall" {
@@ -75,15 +74,18 @@ resource "hcloud_firewall" "internal_net_firewall" {
 
 resource "hcloud_server" "internal_net" {
   name        = "internal-net-coreos-2gb-nbg1-1"
-  image       = data.hcloud_image.coreos_snapshot.id
+  image       = "docker-ce"
   server_type = "cpx11"
   location    = "nbg1"
   backups     = false
-  user_data   = data.external.ignition.result.ign
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+  ssh_keys = [
+    hcloud_ssh_key.fedora.id,
+    hcloud_ssh_key.ipadpro.id
+  ]
 }
 
 resource "hcloud_volume" "internal_net_vol" {
@@ -107,6 +109,15 @@ resource "hcloud_volume_attachment" "internal_net_vol_attachment" {
   volume_id = hcloud_volume.internal_net_vol.id
   server_id = hcloud_server.internal_net.id
   automount = true
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "nginx"
 }
 
 resource "synology_container_project" "nginx" {
