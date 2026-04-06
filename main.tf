d59b67dc852113883d247e23fa0a1320aa0380b4
Russell <russell@replo.de>
2026-04-06 13:29:57 +0200

    Set up cloudinit, pangolin containers, bump synology provider

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     synology = {
       source  = "synology-community/synology"
-      version = "~> 0.4.0"
+      version = "~> 0.5.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
@@ -87,10 +87,14 @@ resource "hcloud_server" "internal_net" {
   server_type = "cax11"
   location    = "nbg1"
   backups     = false
+
+  user_data = file("config/cloudinit.yml")
+
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+
   ssh_keys = [
     hcloud_ssh_key.fedora.id,
     hcloud_ssh_key.ipadpro.id,
@@ -119,20 +123,6 @@ resource "hcloud_volume_attachment" "internal_net_vol_attachment" {
   volume_id = hcloud_volume.internal_net_vol.id
   server_id = hcloud_server.internal_net.id
   automount = true
-}
-
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "nginx"
-
-  ports {
-    internal = "80"
-    external = "80"
-  }
 }
 
 resource "synology_container_project" "nginx" {
@@ -165,4 +155,141 @@ resource "cloudflare_dns_record" "wildcard_replo_de_dns_cname_record" {
   proxied = false
   ttl     = 1
   type    = "CNAME"
+}
+
+# Network equivalent to `networks: default: name: pangolin`
+resource "docker_network" "pangolin" {
+  name   = "pangolin"
+  driver = "bridge"
+}
+
+# Pull images
+resource "docker_image" "pangolin" {
+  name = "fosrl/pangolin:latest"
+}
+
+resource "docker_image" "gerbil" {
+  name = "fosrl/gerbil:latest"
+}
+
+resource "docker_image" "traefik" {
+  name = "traefik:v3.6"
+}
+
+# Pangolin container with healthcheck and config volume
+resource "docker_container" "pangolin" {
+  name    = "pangolin"
+  image   = docker_image.pangolin.image_id
+  restart = "unless-stopped"
+
+  # mount config volume into /app/config as in compose
+  volumes {
+    container_path = "/app/config"
+    host_path      = "/var/lib/containers/pangolin/config"
+  }
+
+  # healthcheck
+  healthcheck {
+    test     = ["CMD", "curl", "-f", "http://localhost:3001/api/v1/"]
+    interval = "10s"
+    timeout  = "10s"
+    retries  = 15
+  }
+
+  networks_advanced {
+    name = docker_network.pangolin.name
+  }
+}
+
+# Gerbil container that depends on pangolin (Terraform depends_on ensures create order,
+# but note it doesn't enforce Docker service health status)
+resource "docker_container" "gerbil" {
+  name    = "gerbil"
+  image   = docker_image.gerbil.image_id
+  restart = "unless-stopped"
+
+  # command on container (list form)
+  command = [
+    "--reachableAt=http://gerbil:3004",
+    "--generateAndSaveKeyTo=/var/config/key",
+    "--remoteConfig=http://pangolin:3001/api/v1/"
+  ]
+
+  # mount same config volume at /var/config
+  volumes {
+    container_path = "/var/config"
+    host_path      = "/var/lib/containers/gerbil/config"
+  }
+
+  # Capabilities
+  capabilities {
+    add = ["NET_ADMIN", "SYS_MODULE"]
+  }
+
+  # Ports
+  ports {
+    internal = 51820
+    external = 51820
+    protocol = "udp"
+  }
+  ports {
+    internal = 21820
+    external = 21820
+    protocol = "udp"
+  }
+  ports {
+    internal = 443
+    external = 443
+    protocol = "tcp"
+  }
+  ports {
+    internal = 80
+    external = 80
+    protocol = "tcp"
+  }
+
+  networks_advanced {
+    name = docker_network.pangolin.name
+  }
+
+  # ensure gerbil is created after pangolin (note: this doesn't wait for pangolin HEALTHY state)
+  depends_on = [docker_container.pangolin]
+}
+
+# Traefik: uses the network namespace of gerbil (network_mode: service:gerbil)
+resource "docker_container" "traefik" {
+  name    = "traefik"
+  image   = docker_image.traefik.image_id
+  restart = "unless-stopped"
+
+  # Use the service network mode so Traefik binds ports on gerbil's network namespace.
+  # This replicates Compose's `network_mode: service:gerbil`.
+  # network_mode = "container:${docker_container.gerbil.name}"
+
+  networks_advanced {
+    name = docker_network.pangolin.name
+  }
+
+  command = [
+    "--configFile=/etc/traefik/traefik_config.yml"
+  ]
+
+  volumes {
+    container_path = "/etc/traefik"
+    host_path      = "/var/lib/containers/traefik/etc"
+    read_only      = true
+  }
+
+  volumes {
+    container_path = "/letsencrypt"
+    host_path      = "/var/lib/containers/traefik/letsencrypt"
+  }
+
+  volumes {
+    container_path = "/var/log/traefik"
+    host_path      = "/var/lib/containers/traefik/logs"
+  }
+
+  # Make sure Traefik is created after services it depends on
+  depends_on = [docker_container.pangolin, docker_container.gerbil]
 }
