1e910f2788edf2644b66d04cfaabb5a90d2ce0fc
Russell <russell@replo.de>
2026-04-11 01:35:58 +0200

    Bump kanboard

--- s920p.tf
+++ s920p.tf
@@ -917,7 +917,7 @@ resource "synology_container_project" "util" {
 
   services = {
     kan = {
-      image   = "kanboard/kanboard:v1.2.10"
+      image   = "kanboard/kanboard:v1.2.17"
       restart = "unless-stopped"
 
       labels = {
