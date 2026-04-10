14de1f2b0132f422a4f4a5e114472c0d0ad87b96
re-plode <96007673+re-plode@users.noreply.github.com>
2026-04-11 00:40:48 +0200

    Bump kanboard

--- s920p.tf
+++ s920p.tf
@@ -917,7 +917,7 @@ resource "synology_container_project" "util" {
 
   services = {
     kan = {
-      image   = "kanboard/kanboard:v1.2.4"
+      image   = "kanboard/kanboard:v1.2.5"
       restart = "unless-stopped"
 
       labels = {
